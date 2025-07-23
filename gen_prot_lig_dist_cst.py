208b750ce086b3c78d4155e5c464d8c2157760f8
daylight-00 <hwjang00@snu.ac.kr>
2025-07-24 00:50:07 +0900

    Refactor with biopython

--- gen_prot_lig_dist_cst.py
+++ gen_prot_lig_dist_cst.py
@@ -1,101 +1,67 @@
-import os
-import sys
+from Bio.PDB import PDBParser
 import numpy as np
-import math
-sys.path.insert(0,'/home/gyurie/lib')
-from libProtein import three_aa_to_one_aa
-from collections import defaultdict
 
-class Atom:
-    def __init__(self,line):
-        self.raw_line = line
-        self.line = line.strip()
-        self.chainID = line[21]
-        self.atmNo = int(line[7:11])
-        self.atmName = line[11:17].strip()
-        self.resNo = int(line[22:26])
-        self.alt_state = line[16]
-        self.resName = line[17:21].strip()
-        self.oneLetter = three_aa_to_one_aa(self.resName)
-        self.coord = [float(line[30:38]),\
-                      float(line[38:46]),\
-                      float(line[46:54])]
-        #
-        self.rep_resNo = '%s%d'%(self.chainID,self.resNo)
-        self.template = '%s'%(line[:30])
-        self.full_template = line.strip()
-        return
-
-def dist_two_coords(crd_a,crd_b):
-    a = np.array(crd_a)
-    b = np.array(crd_b)
-    d_xyz = a-b
-    d2_sum = []
-    for d in d_xyz:
-        d2_sum.append(d*d)
-    return math.sqrt(sum(d2_sum))
-
-
-#example
-#'AtomPair NE1 11 O 7 HARMONIC 3.0 0.5'
 CST_STDERR = 0.5
 
-#Assume there is one type of hetatm ligand and that is our target
-def extract_dist_cst_from_pdb(pdb_in,lig_tr_atms,bsite_res=''):
-    #
-    #use all_res
+def extract_dist_cst_from_pdb(pdb_in, lig_tr_atms, bsite_res=''):
+    """
+    pdb_in      : PDB 파일 경로
+    lig_tr_atms : ['NE1','O', ...] 등 리간드에서 관심있는 atom 이름 리스트
+    bsite_res   : '' 이면 모든 단백질 CA 사용, 아니면 '10,15,27' 처럼 콤마로 구분된 residue 번호 문자열
+    return      : ['AtomPair <lig_atm> <het_resno> CA <closest_resno> HARMONIC <dist> 0.5', ...]
+    """
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure("strc", pdb_in)
+
+    # 체인/모델은 하나라고 가정(필요시 loop 추가)
+    model = next(structure.get_models())
+
+    # bsite residue set
     if bsite_res == '':
-        bsite_res_s = []
-        with open(pdb_in) as fp:
-            for line in fp:
-                if line.startswith('ATOM') and line[13:15] == 'CA':
-                    bsite_res_s.append(int(line[22:26]))
+        # 모든 단백질 CA residue 번호
+        bsite_res_s = {res.get_id()[1] for res in model.get_residues()
+                       if res.get_id()[0] == ' ' and 'CA' in res}  # ' ' => ATOM
     else:
-        bsite_res_s = [int(x) for x in bsite_res.split(',')]                    
-        
-    #Will get distances between each ligand target atom and the closest binding site residue CA
-    #First read ligand target atom and binding site residue CA coords
+        bsite_res_s = {int(x) for x in bsite_res.split(',')}
+
+    # 단백질 마지막 residue 번호(리간드 resno 추정용)
+    last_prot_resno = max(res.get_id()[1] for res in model.get_residues()
+                          if res.get_id()[0] == ' ')
+
+    # CA 좌표 dict
     bsite_CA_R = {}
+    for res in model.get_residues():
+        if res.get_id()[0] == ' ' and res.get_id()[1] in bsite_res_s and 'CA' in res:
+            bsite_CA_R[res.get_id()[1]] = res['CA'].get_coord()
+
+    # 리간드 atom 좌표 dict
     het_atm_R = {}
-    het_resno = None
-    #in case the ligand residue is not numbered sequentially (e.g. diffusion output)
-    last_prot_resno = 0
-    with open(pdb_in) as fp:
-        for line in fp:
-            if line.startswith('ATOM'):
-                atm = Atom(line.strip())
-                if atm.resNo in bsite_res_s and atm.atmName == 'CA':
-                    bsite_CA_R[atm.resNo] = atm.coord
-                last_prot_resno = atm.resNo
-            elif line.startswith('HETATM'):
-                atm = Atom(line.strip())
-#                het_resno = atm.resNo
-                if atm.atmName in lig_tr_atms:
-                    het_atm_R[atm.atmName] = atm.coord
-    #
-    #in case the ligand residue is not numbered sequentially (e.g. diffusion output)
-    het_resno = last_prot_resno + 1
-    #for each ligand target atm get distances from each bsite residue CA and sort
+    for res in model.get_residues():
+        if res.get_id()[0].startswith('H'):  # HETATM
+            for atom in res:
+                if atom.get_name() in lig_tr_atms:
+                    het_atm_R[atom.get_name()] = atom.get_coord()
+
+    het_resno = last_prot_resno + 1  # diffusion 등 불연속 번호 대응
+
+    # 거리 계산 (벡터화)
     cst_s = []
-    for hetatm in lig_tr_atms:
-        het_R = het_atm_R[hetatm]
-        #
-        het_CA_d = {}
-        for resno in bsite_res_s:
-            CA_R = bsite_CA_R[resno]
-            d = dist_two_coords(het_R,CA_R)
-            het_CA_d[resno] = d
-        #
-        sorted_ds = sorted(het_CA_d.items(), key=lambda item: item[1])
-        closest = sorted_ds[0]
-        closest_resno = closest[0]
-        closest_d = closest[1]
-        #
-        
-        cst_line = f'AtomPair {hetatm} {het_resno} CA {closest_resno} HARMONIC {closest_d} {CST_STDERR}'
+    bsite_resnos = np.array(list(bsite_CA_R.keys()))
+    bsite_coords = np.array([bsite_CA_R[r] for r in bsite_resnos])
+
+    for atm_name in lig_tr_atms:
+        het_coord = het_atm_R[atm_name]
+
+        # 모든 CA와의 거리
+        diff = bsite_coords - het_coord
+        dists = np.sqrt((diff ** 2).sum(axis=1))
+
+        # 최단 거리 residue
+        idx = np.argmin(dists)
+        closest_resno = int(bsite_resnos[idx])
+        closest_d = float(dists[idx])
+
+        cst_line = f'AtomPair {atm_name} {het_resno} CA {closest_resno} HARMONIC {closest_d} {CST_STDERR}'
         cst_s.append(cst_line)
+
     return cst_s
-        
-                
-                    
-    
